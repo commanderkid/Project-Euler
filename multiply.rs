885b8269f06d0a4fa35918440ba5067675ad0b30
Alexey <nafiguspofigus@gmail.com>
2026-07-06 09:43:40 +0300

    Create multiply.rs

---
+++ multiply.rs
@@ -0,0 +1,16 @@
+const MAX_NUMBER: u32 = 1000;
+
+fn main() {
+    
+    let mut summa: u32 = 0;
+    
+    for i in (0..MAX_NUMBER).step_by(3) {
+        summa += i;
+    }
+    
+    for j in (0..MAX_NUMBER).step_by(5) {
+        summa += j;
+    }
+    
+    println!("{}", summa); //266333
+}
